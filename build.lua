026b5721584ac684e0c9fe0895e7fc58db3c2ad6
Quejiahao <quejiahao@live.com>
2021-03-30 18:38:40 +0800

    Revise for Beijing Normal University bachelor degree thesis

---
+++ build.lua
@@ -0,0 +1,98 @@
+#!/usr/bin/env texlua
+
+module = "bnuthesis"
+
+supportdir = "./testfiles/support"
+checksuppfiles = {"*.tex"}
+
+demofiles = {"latexmkrc", "Makefile"}
+docfiles = {
+  "CHANGELOG.md",
+  "bnusetup.tex",
+  "data", "ref",
+}
+installfiles = {"*.cls", "*.bst", "*.bbx", "*.cbx", "tsinghua-name-bachelor.pdf"}
+sourcefiles = {"*.dtx", "*.ins", "*.bst", "*.bbx", "*.cbx", "tsinghua-name-bachelor.pdf"}
+tagfiles = {"*.dtx", "CHANGELOG.md", "package.json"}
+textfiles = {"*.md","LICENSE"}
+typesetdemofiles = {"bnuthesis-example.tex", "spine.tex"}
+
+excludetests = {
+  "06-*",
+  "07-*",
+  "09-*",
+}
+
+checkengines = {"xetex"}
+stdengine = "xetex"
+
+checkconfigs = {
+  "build",
+  "testfiles/config-crossref",
+  "testfiles/config-nomencl",
+  "testfiles/config-bibtex",
+  "testfiles/config-biblatex",
+}
+
+-- include .tds.zip in build output
+packtdszip = true
+
+-- specify some files of their correct TDS locations
+tdslocations = {
+  "bibtex/bst/bnuthesis/*.bst",
+  "tex/latex/bnuthesis/tsinghua-name-bachelor.pdf",
+}
+
+typesetexe = "xelatex"
+unpackexe = "xetex"
+bibtexexe = "bibtex"
+bibtexopts = ""
+biberopts = "--quiet"
+
+checkopts = "-file-line-error -halt-on-error -interaction=nonstopmode"
+typesetopts = "-shell-escape -file-line-error -halt-on-error -interaction=nonstopmode"
+
+lvtext = ".tex"
+
+function docinit_hook()
+  for _, file in pairs({"dtx-style.sty"}) do
+    cp(file, unpackdir, typesetdir)
+  end
+  return 0
+end
+
+function update_tag(file, content, tagname, tagdate)
+  local url = "https://github.com/Quejiahao/bnuthesis"
+  local date = string.gsub(tagdate, "%-", "/")
+  if string.match(file, "%.dtx$") then
+    if string.match(content, "%d%d%d%d/%d%d/%d%d [0-9.]+") then
+      content = string.gsub(content, "%d%d%d%d/%d%d/%d%d [0-9.]+",
+        date .. " " .. tagname)
+    end
+    if string.match(content, "\\def\\version{[0-9.]+}") then
+      content = string.gsub(content, "\\def\\version{[0-9.]+}",
+        "\\def\\version{" .. tagname .. "}")
+    end
+
+  elseif string.match(file, "CHANGELOG.md") then
+    local previous = string.match(content, "/compare/(.*)%.%.%.HEAD")
+    local gittag = 'v' .. tagname
+
+    if gittag == previous then return content end
+    content = string.gsub(content,
+      "## %[Unreleased%]",
+      "## [Unreleased]\n\n## [" .. gittag .."] - " .. tagdate)
+    content = string.gsub(content,
+      previous .. "%.%.%.HEAD",
+      gittag .. "...HEAD\n" ..
+      string.format("%-14s", "[" .. gittag .. "]:") .. url .. "/compare/"
+        .. previous .. "..." .. gittag)
+
+  elseif string.match(file, "package.json") then
+    content = string.gsub(content,
+      "\"version\": \"[0-9.]+\"",
+      "\"version\": \"" .. tagname .. "\"")
+  end
+
+  return content
+end
---
+++ testfiles/save.sh
@@ -0,0 +1,46 @@
+save_config ()
+    if [[ "$1" == "main" ]]; then
+        for testfile in testfiles/*.tex; do
+            test="$(basename "$testfile" .tex)";
+            if ! [[ "$test" == 06-* || "$test" == 07-* || "$test" == 09-* ]]; then
+                l3build save --quiet "$test" || exit 1;
+            fi
+        done
+
+    elif [[ "$1" == "crossref" ]]; then
+        for testfile in testfiles/*.tex ; do
+            test="$(basename "$testfile" .tex)";
+            if [[ "$test" == 06-* || "$test" == 07-* || "$test" == 09-* ]]; then
+                l3build save --quiet --config testfiles/config-crossref "$test" || exit 1;
+            fi
+        done
+
+    elif [[ "$1" == "nomencl" ]]; then
+        for testfile in testfiles/08-notation-nomencl/*.tex; do
+            test="$(basename "$testfile" .tex)";
+            l3build save --quiet --config testfiles/config-nomencl "$test" || exit 1;
+        done
+
+    elif [[ "$1" == "bibtex" ]]; then
+        for testfile in testfiles/10-bibtex/*.tex; do
+            test="$(basename "$testfile" .tex)";
+            l3build save --quiet --config testfiles/config-bibtex "$test" || exit 1;
+        done
+
+    elif [[ "$1" == "biblatex" ]]; then
+        for testfile in testfiles/10-biblatex/*.tex; do
+            test="$(basename "$testfile" .tex)";
+            l3build save --quiet --config testfiles/config-biblatex "$test" || exit 1;
+        done
+    fi
+
+
+
+
+if [[ $# -eq 0 ]]; then
+    for config in main crossref nomencl bibtex biblatex; do
+        save_config $config;
+    done
+else
+    save_config $1;
+fi
